e931bb866186d2829e28104611c9f233f3e44fec
ANJALI PA <anjali9265@gmail.com>
2023-03-02 08:53:58 +0100

    add check to currentCount

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -14,13 +14,15 @@ export default async function handler(
 ) {
   if (req.method === 'POST') {
     const currentCount = await prisma.counter.findMany()
-    const count = currentCount?.pop()?.visitCount + 1
-    const result = await prisma.counter.update({
-      where: { id: 1 },
-      data: { visitCount: count }
-    })
-    res.status(200).send(result);
-    return
+    if (!!currentCount?.pop()?.visitCount) {
+      const count = currentCount.pop().visitCount + 1
+      const result = await prisma.counter.update({
+        where: { id: 1 },
+        data: { visitCount: count }
+      })
+      res.status(200).send(result);
+      return
+    }
   } else {
     const currentCount = await prisma.counter.findMany()
     return res.status(200).send(currentCount);
